22c903d35678ae56e4f5c28a7020797375ade793
yagoalessio88 <yagoalessio88@gmail.com>
2021-11-09 16:45:53 +0100

    general upload

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita
+        voluptas sequi eveniet ullam, voluptatum inventore veritatis vitae.
+        Ullam in iusto hic eligendi tempore nostrum debitis sint esse cupiditate
+        sed reprehenderit, fugiat voluptatibus ea ratione eaque. Id totam ipsam
+        est iure!
+      </p>
+    </section>
+  );
+};
+
+export default About;
